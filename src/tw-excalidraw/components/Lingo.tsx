2ae363aa7e03c53c4a6ab5a739e2edd4e1ac4d47
ItsTheWhale <103934114+ItsTheWhale@users.noreply.github.com>
2026-04-07 23:12:40 +0800

    refactor: remove unnecessary tw-excalidraw-lingo functionality

--- src/tw-excalidraw/components/Lingo.tsx
+++ src/tw-excalidraw/components/Lingo.tsx
@@ -3,5 +3,5 @@ import { Wikify } from './Wikify.js';
 export function Lingo({ title }: {
   title: string;
 }) {
-  return <Wikify text={`<<tw-excalidraw-lingo ${title} $:/plugins/itw/tw-excalidraw/language/>>`} />;
+  return <Wikify text={`<<tw-excalidraw-lingo ${title}>>`} />;
 }
