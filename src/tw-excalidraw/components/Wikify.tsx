306eda6b7939a02c366b7db46a7ba6f93f832d40
ItsTheWhale <103934114+ItsTheWhale@users.noreply.github.com>
2026-07-09 21:26:59 +0800

    fix: duplicating widgets

--- src/tw-excalidraw/components/Wikify.tsx
+++ src/tw-excalidraw/components/Wikify.tsx
@@ -1,15 +1,19 @@
 import { useWidget } from '$:/plugins/linonetwo/tw-react/index.js';
 
+import type { IParseTreeNode } from 'tiddlywiki';
+
 import type { JSX, RefObject } from 'react';
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 
 export function Wikify({ text }: { text: string }): JSX.Element {
   const transcludeContainerReference = useRef<HTMLDivElement>(null);
 
-  const parseTreeNode = {
-    type: 'tiddler',
-    children: $tw.wiki.parseText('text/vnd.tiddlywiki', text).tree,
-  };
+  const parseTreeNode = useMemo<IParseTreeNode>(() => {
+    return {
+      type: 'tiddler',
+      children: $tw.wiki.parseText('text/vnd.tiddlywiki', text).tree,
+    };
+  }, [text]);
 
   // While useWidget accepts a nullable ref, this is not reflected in its type
   useWidget(parseTreeNode, transcludeContainerReference as RefObject<HTMLDivElement>, {
